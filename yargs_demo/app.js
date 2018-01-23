085532078013371b46674f48320e17a38aba9c5d
daiwenjuan <1581413545@qq.com>
2018-01-23 19:53:36 +0800

    yargstest

---
+++ yargs_demo/app.js
@@ -0,0 +1,14 @@
+/**
+ *  Created by daiwenjuan on 2018/1/23 上午10:45.
+ */
+const a = require('yargsdemodwj')
+// function test() {
+//   if (argv.l == 'zh-cn') {
+//     console.log('zh_cn')
+//   } else if (argv.l == 'en') {
+//     console.log('en')
+//   }
+// }
+// test()
+//node app.js --l=zh-cn      zh_cn
+//node app.js --l=en           en
